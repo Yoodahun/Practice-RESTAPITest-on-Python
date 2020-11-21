26c316fd6e2358318287e089e8d180dda4192eb2
Yoodahun <dahun4032@gmail.com>
2020-11-21 22:07:26 +0900

    Refactoring DBConnection (make method)

--- DBExample.py
+++ DBExample.py
@@ -1,19 +1,20 @@
 import mysql.connector
+from utilities.configurations import *
 
-#hostserver, database_name, username and password
-conn = mysql.connector.connect(host='localhost', database='APIDevelop', user='root', password='')
+# hostserver, database_name, username and password
+conn = getSQLConnection()
 
-print(conn.is_connected()) #true
-db_cursor = conn.cursor() #instance for execute sql
+print(conn.is_connected())  # true
+db_cursor = conn.cursor()  # instance for execute sql
 
-#select data from table
+# select data from table
 db_cursor.execute('select * from CustomerInfo')
 # row = db_cursor.fetchone() #first row in result
 # print(row) #tuple.
 # print(row[3]) #print element.
 
-row_all = db_cursor.fetchall() #all row in result
-print(row_all) # list of tuples
+row_all = db_cursor.fetchall()  # all row in result
+print(row_all)  # list of tuples
 print(row_all[0][3])
 
 sum = 0
--- utilities/configurations.py
+++ utilities/configurations.py
@@ -1,4 +1,6 @@
 import configparser
+import mysql.connector
+from mysql.connector import Error
 
 
 def getConfig():
@@ -6,3 +8,19 @@ def getConfig():
     config.read("utilities/properties.ini")
     return config
 
+
+def getSQLConnection():
+    connection_data = getConfig()['SQL']
+
+    try:
+        sql_connection = mysql.connector.connect(
+            host=connection_data['host'],
+            database=connection_data['database'],
+            user=connection_data['user'],
+            password=connection_data['password']
+        )
+        if sql_connection.is_connected():
+            print("Connection Successful")
+            return sql_connection
+    except Error as e:
+        print(e)
